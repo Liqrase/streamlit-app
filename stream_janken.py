611edb8a35315d1b62accc3659314d637d01814a
Liqrase <qarnqitana9w4@gmail.com>
2025-03-06 20:07:28 +0900

    修正内容の説明（例: Streamlitアプリのバグ修正）

--- stream_janken.py
+++ stream_janken.py
@@ -50,7 +50,8 @@ if st.button("じゃんけん！"):
         st.markdown(f"#### あいこ。やり直し！")
     
     st.write(f"現在地：あなた {st.session_state.p_walk} マス、CPU {st.session_state.cp_walk} マス")
-    st.write("続けて出したい手を選んでください")
+while st.session_state.p_walk < total_distance:
+    st.write("続けて出したい手を選んでください。")
 
 if st.session_state.p_walk >= total_distance:
     pl1, pl2 = st.columns(2)
